03fee72c2aaadc523b7a331eb9f34c43a36dff7a
Delgan <adrien.gabillaud@gmail.com>
2017-09-04 23:23:15 +0200

    Publish v0.0.1 to PyPi

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'loguru',
+  packages = ['loguru'],
+  version = '0.0.1',
+  description = 'Logging as an automatism',
+  author = 'Delgan',
+  author_email = 'delgan.py@gmail.com',
+  url = 'https://github.com/Delgan/loguru',
+  download_url = 'https://github.com/Delgan/loguru/archive/0.0.1.tar.gz',
+  keywords = ['logging'],
+  classifiers = [],
+)
