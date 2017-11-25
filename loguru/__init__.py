93c1189e1ce30a7537fe22a431253e271614ac41
Delgan <delgan.py@gmail.com>
2017-11-25 11:50:19 +0100

    Log to stderr only if available (see issue 13807 of Python bug tracker)

--- loguru/__init__.py
+++ loguru/__init__.py
@@ -6,6 +6,8 @@ from ._logger import Logger as _Logger
 __version__ = "0.0.1"
 
 logger = _Logger()
-logger.start(_sys.stderr)
+
+if _sys.stderr:
+    logger.start(_sys.stderr)
 
 _atexit.register(logger.stop)
