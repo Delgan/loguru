203a37dd389184f9d985f680849c83c562a13a02
Delgan <delgan.py@gmail.com>
2018-03-16 21:42:04 +0100

    Simplify "propagate()" snippet

--- tests/test_propagation.py
+++ tests/test_propagation.py
@@ -6,15 +6,13 @@ from logging import StreamHandler
 
 def propagate_sink(message):
     record = message.record
-    logging_logger = logging.getLogger(record['name'])
-    logging_record = logging_logger.makeRecord(
-        record['name'], record['level'].no, record['file'].path, record['line'], record['message'],
-        [], message.exception, record['function'], logger.extra, None)
-    logging_logger.handle(logging_record)
+    log = logging.getLogger(record['name'])
+    log.findCaller = lambda _: (record['file'].path, record['line'], record['function'], None)
+    log.log(record['level'].no, record['message'], exc_info=message.exception)
 
 def test_formatting(make_logging_logger, capsys):
     fmt = "%(name)s - %(filename)s - %(funcName)s - %(levelname)s - %(levelno)s - %(lineno)d - %(module)s - %(message)s"
-    expected = "tests.test_propagation - test_propagation.py - test_formatting - DEBUG - 10 - 20 - test_propagation - This is my message\n"
+    expected = "tests.test_propagation - test_propagation.py - test_formatting - DEBUG - 10 - 18 - test_propagation - This is my message\n"
     logging_logger = make_logging_logger(None, StreamHandler(sys.stderr), fmt)
     logger.start(propagate_sink)
     logger.debug("This {verb} my {}", "message", verb="is")
