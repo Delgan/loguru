445edf4977325add84133731373d8aa88a2e4257
Delgan <delgan.py@gmail.com>
2018-04-09 18:01:49 +0200

    Use "setup.py" to declare "dev" dependencies

--- setup.py
+++ setup.py
@@ -12,25 +12,31 @@ with open('README.rst', 'rb') as file:
     readme = file.read().decode('utf-8')
 
 setup(
-    name = 'loguru',
-    version = version,
-    description = 'Python logging made (stupidly) simple',
-    long_description = readme,
-    author = 'Delgan',
-    author_email = 'delgan.py@gmail.com',
-    url = 'https://github.com/Delgan/loguru',
-    download_url = 'https://github.com/Delgan/loguru/archive/{}.tar.gz'.format(version),
-    keywords = ['loguru', 'logging', 'logger', 'log'],
+    name='loguru',
+    version=version,
+    description='Python logging made (stupidly) simple',
+    long_description=readme,
+    author='Delgan',
+    author_email='delgan.py@gmail.com',
+    url='https://github.com/Delgan/loguru',
+    download_url='https://github.com/Delgan/loguru/archive/{}.tar.gz'.format(version),
+    keywords=['loguru', 'logging', 'logger', 'log'],
     license="MIT license",
-    classifiers = [],
-    install_requires = [
+    install_requires=[
         'ansimarkup>=1.3.0',
         'base36>=0.1.1',
         'better_exceptions_fork>=0.2.1.post5',
         'colorama>=0.3.9',
         'pendulum>=1.4.2',
     ],
-    ext_modules = [
+    extras_require={
+        'dev': [
+            'coveralls>=1.3.0',
+            'pytest>=3.5.0',
+            'pytest-cov>=2.5.1',
+        ],
+    },
+    ext_modules=[
         Extension('loguru._extensions.fast_now', sources=['loguru/_extensions/fast_now.c'], optional=True)
     ]
 )
