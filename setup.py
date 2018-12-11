77a971dafb2c123113e021389c2d221bc2c31065
Unviray <unviray@gmail.com>
2018-12-11 16:53:13 +0300

    prevent setup.py to run with import

--- setup.py
+++ setup.py
@@ -13,46 +13,52 @@ with open("loguru/__init__.py", "r") as file:
 with open("README.rst", "rb") as file:
     readme = file.read().decode("utf-8")
 
-setup(
-    name="loguru",
-    version=version,
-    packages=["loguru"],
-    description="Python logging made (stupidly) simple",
-    long_description=readme,
-    author="Delgan",
-    author_email="delgan.py@gmail.com",
-    url="https://github.com/Delgan/loguru",
-    download_url="https://github.com/Delgan/loguru/archive/{}.tar.gz".format(version),
-    keywords=["loguru", "logging", "logger", "log", "parser"],
-    license="MIT license",
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "Natural Language :: English",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: Implementation :: CPython",
-    ],
-    install_requires=[
-        "ansimarkup>=1.4.0",
-        "better_exceptions_fork>=0.2.1.post6",
-        "colorama>=0.4.1",
-    ],
-    extras_require={
-        "dev": [
-            "black>=18.6b4",
-            "coveralls>=1.3.0",
-            "isort>=4.3.4",
-            "pytest>=3.5.0",
-            "pytest-cov>=2.5.1",
-            "Sphinx>=1.7.4",
-            "sphinx-autobuild>=0.7",
-            "sphinx-rtd-theme>=0.3",
-        ]
-    },
-    python_requires=">=3.6",
-)
+
+def install():
+    setup(
+        name="loguru",
+        version=version,
+        packages=["loguru"],
+        description="Python logging made (stupidly) simple",
+        long_description=readme,
+        author="Delgan",
+        author_email="delgan.py@gmail.com",
+        url="https://github.com/Delgan/loguru",
+        download_url="https://github.com/Delgan/loguru/archive/{}.tar.gz".format(version),
+        keywords=["loguru", "logging", "logger", "log", "parser"],
+        license="MIT license",
+        classifiers=[
+            "Development Status :: 5 - Production/Stable",
+            "Intended Audience :: Developers",
+            "Natural Language :: English",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: Implementation :: CPython",
+        ],
+        install_requires=[
+            "ansimarkup>=1.4.0",
+            "better_exceptions_fork>=0.2.1.post6",
+            "colorama>=0.4.1",
+        ],
+        extras_require={
+            "dev": [
+                "black>=18.6b4",
+                "coveralls>=1.3.0",
+                "isort>=4.3.4",
+                "pytest>=3.5.0",
+                "pytest-cov>=2.5.1",
+                "Sphinx>=1.7.4",
+                "sphinx-autobuild>=0.7",
+                "sphinx-rtd-theme>=0.3",
+            ]
+        },
+        python_requires=">=3.6",
+    )
+
+
+if __name__ == '__main__':
+    install()
