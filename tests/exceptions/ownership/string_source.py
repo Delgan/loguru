d66ad644615cc8c996135c7e100e081f6dbfa585
Delgan <delgan.py@gmail.com>
2019-03-17 15:45:37 +0100

    Add coloration of stacktrace regardless "backtrace" / "diagnose" options

--- tests/exceptions/ownership/string_source.py
+++ tests/exceptions/ownership/string_source.py
@@ -1,13 +1,21 @@
 import sys
 from loguru import logger
 
-logger.remove()
-logger.add(sys.stderr, format="", colorize=True, backtrace=True, diagnose=True)
 
-def foo():
-    1 / 0
+def test(*, backtrace, colorize, diagnose):
+    logger.remove()
+    logger.add(sys.stderr, format="", colorize=colorize, backtrace=backtrace, diagnose=diagnose)
 
-try:
-    exec("foo()")
-except ZeroDivisionError:
-    logger.exception("")
+    def foo():
+        1 / 0
+
+    try:
+        exec("foo()")
+    except ZeroDivisionError:
+        logger.exception("")
+
+
+test(backtrace=True, colorize=True, diagnose=True)
+test(backtrace=True, colorize=True, diagnose=False)
+test(backtrace=False, colorize=True, diagnose=False)
+test(backtrace=False, colorize=False, diagnose=False)
