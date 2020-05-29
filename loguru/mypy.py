d52cfc09523f7080ec9178fd3c0f1f5b75bf401b
Tomasz Trębski <kornicameister@gmail.com>
2020-05-30 00:12:33 +0200

    fixup! Implement mypy plugin for loguru

--- loguru/mypy.py
+++ loguru/mypy.py
@@ -1,7 +1,8 @@
 import string
 import typing as t
 
-from mypy.nodes import StrExpr
+from mypy.checker import TypeChecker
+from mypy.nodes import LambdaExpr, StrExpr, NameExpr, FuncItem
 from mypy.plugin import MethodContext, Plugin
 from mypy.errorcodes import ErrorCode
 from mypy.types import Type
@@ -19,13 +20,27 @@ ERROR_BAD_KWARG: t.Final[ErrorCode] = ErrorCode(
 )
 
 
-def _loguru_logger_call_handler(ctx: MethodContext,) -> Type:
+def _loguru_logger_call_handler(ctx: MethodContext) -> Type:
     log_msg_expr = ctx.args[0][0]
     assert isinstance(log_msg_expr, StrExpr)
+    assert isinstance(ctx.api, TypeChecker)
 
     # collect call args/kwargs
-    call_args_count = len(ctx.args[1])
-    call_kwargs = {kwarg_name: ctx.args[2][idx] for idx,kwarg_name in enumerate(ctx.arg_names[2])}
+    # due to funky structure mypy offers here, it's easier
+    # to beg for forgiveness here
+    try:
+        call_args = ctx.args[1]
+        call_args_count = len(call_args)
+    except IndexError:
+        call_args = []
+        call_args_count = 0
+    try:
+        call_kwargs = {
+            kwarg_name: ctx.args[2][idx]
+            for idx, kwarg_name in enumerate(ctx.arg_names[2])
+        }
+    except IndexError:
+        call_kwargs = {}
 
     # collect args/kwargs from string interpolation
     log_msg_value: str = log_msg_expr.value
@@ -41,31 +56,70 @@ def _loguru_logger_call_handler(ctx: MethodContext,) -> Type:
 
     if log_msg_expected_args_count > call_args_count:
         ctx.api.msg.fail(
-            f"Missing {log_msg_expected_args_count - call_args_count} positional arguments for log message",
+            f"Missing {log_msg_expected_args_count - call_args_count} "
+            "positional arguments for log message",
             context=log_msg_expr,
             code=ERROR_BAD_ARG,
         )
         return ctx.default_return_type
     elif log_msg_expected_args_count < call_args_count:
-        ctx.api.msg.warn(
-            f"Expected {log_msg_expected_args_count}, but found {call_args_count} positional arguments for log message",
+        ctx.api.msg.note(
+            f"Expected {log_msg_expected_args_count} but found {call_args_count} "
+            "positional arguments for log message",
             context=log_msg_expr,
             code=ERROR_BAD_ARG,
         )
         return ctx.default_return_type
+    else:
+        for call_pos, call_arg in enumerate(call_args):
+            if isinstance(call_arg, LambdaExpr) and call_arg.arguments:
+                ctx.api.msg.fail(
+                    f"Expected 0 arguments for <lambda>: {call_pos} arg ",
+                    context=call_arg,
+                    code=ERROR_BAD_ARG,
+                )
+            elif (
+                isinstance(call_arg, NameExpr)
+                and isinstance(call_arg.node, FuncItem)
+                and call_arg.node.arguments
+            ):
+                ctx.api.msg.fail(
+                    f"Expected 0 arguments for {call_arg.node.fullname}: {call_arg}",
+                    context=call_arg,
+                    code=ERROR_BAD_ARG,
+                )
 
     for log_msg_kwarg in log_msg_expected_kwargs:
         maybe_kwarg_expr = call_kwargs.pop(log_msg_kwarg, None)
         if maybe_kwarg_expr is None:
             ctx.api.msg.fail(
-                f"{log_msg_kwarg} keyword argument is missing", context=log_msg_expr, code=ERROR_BAD_KWARG,
+                f"{log_msg_kwarg} keyword argument is missing",
+                context=log_msg_expr,
+                code=ERROR_BAD_KWARG,
             )
             return ctx.default_return_type
-        # TODO add kwarg type being lambda or any other callable
+        elif isinstance(maybe_kwarg_expr, LambdaExpr) and maybe_kwarg_expr.arguments:
+            ctx.api.msg.fail(
+                f"Expected 0 arguments for <lambda>: {log_msg_kwarg} kwarg ",
+                context=maybe_kwarg_expr,
+                code=ERROR_BAD_KWARG,
+            )
+        elif (
+            isinstance(maybe_kwarg_expr, NameExpr)
+            and isinstance(maybe_kwarg_expr.node, FuncItem)
+            and maybe_kwarg_expr.node.arguments
+        ):
+            ctx.api.msg.fail(
+                f"Expected 0 arguments for {maybe_kwarg_expr.node.fullname}: {log_msg_kwarg}",
+                context=maybe_kwarg_expr,
+                code=ERROR_BAD_KWARG,
+            )
 
-    if call_kwargs:
+    for extra_kwarg_name in call_kwargs:
         ctx.api.msg.fail(
-            f"{call_kwargs.keys()} keyword argument are not present in log message", context=log_msg_expr, code=ERROR_BAD_KWARG,
+            f"{extra_kwarg_name} keyword argument not found in log message",
+            context=log_msg_expr,
+            code=ERROR_BAD_KWARG,
         )
 
     return ctx.default_return_type
@@ -75,7 +129,7 @@ class LoguruPlugin(Plugin):
     builtin_severities = (
         "info",
         "debug",
-        "trace",
+        "warning",
         "error",
         "exception",
     )
