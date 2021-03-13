ebbf89c436e506d2e5c5fcd67ed8ea70d3ed8739
Delgan <delgan.py@gmail.com>
2021-03-13 15:31:38 +0100

    Add support and CI tests for Python 3.9

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: Implementation :: PyPy",
         "Programming Language :: Python :: Implementation :: CPython",
