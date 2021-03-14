e95c89f0214c5089d4a4d358951f81b3c2a09482
Delgan <delgan.py@gmail.com>
2021-03-14 21:40:18 +0100

    Optimize "filter" function when using a dict (#407)

--- loguru/_filters.py
+++ loguru/_filters.py
@@ -12,21 +12,13 @@ def filter_by_name(record, parent, length):
 def filter_by_level(record, level_per_module):
     name = record["name"]
 
-    level = 0
-
-    if name in level_per_module:
-        level = level_per_module[name]
-    elif name is not None:
-        lookup = ""
-        if "" in level_per_module:
-            level = level_per_module[""]
-        for n in name.split("."):
-            lookup += n
-            if lookup in level_per_module:
-                level = level_per_module[lookup]
-            lookup += "."
-
-    if level is False:
-        return False
-
-    return record["level"].no >= level
+    while True:
+        level = level_per_module.get(name, None)
+        if level is False:
+            return False
+        if level is not None:
+            return record["level"].no >= level
+        if not name:
+            return True
+        index = name.rfind(".")
+        name = name[:index] if index != -1 else ""
