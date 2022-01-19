311a04c450fe3d2b0a7f33373aaed45860aa6d44
Delgan <delgan.py@gmail.com>
2022-01-19 19:42:38 +0100

    Remove dangerous use of "importlib.reload()" in tests
    It changes the global state and may cause unexpected behavior.

--- loguru/_ctime_functions.py
+++ loguru/_ctime_functions.py
@@ -1,49 +1,53 @@
 import os
 
-if os.name == "nt":
-    import win32_setctime
 
-    def get_ctime(filepath):
-        return os.stat(filepath).st_ctime
+def load_ctime_functions():
+    if os.name == "nt":
+        import win32_setctime
 
-    def set_ctime(filepath, timestamp):
-        if not win32_setctime.SUPPORTED:
-            return
+        def get_ctime(filepath):
+            return os.stat(filepath).st_ctime
 
-        try:
-            win32_setctime.setctime(filepath, timestamp)
-        except (OSError, ValueError):
+        def set_ctime(filepath, timestamp):
+            if not win32_setctime.SUPPORTED:
+                return
+
+            try:
+                win32_setctime.setctime(filepath, timestamp)
+            except (OSError, ValueError):
+                pass
+
+    elif hasattr(os.stat_result, "st_birthtime"):
+
+        def get_ctime(filepath):
+            return os.stat(filepath).st_birthtime
+
+        def set_ctime(filepath, timestamp):
             pass
 
+    elif hasattr(os, "getxattr") and hasattr(os, "setxattr"):
 
-elif hasattr(os.stat_result, "st_birthtime"):
+        def get_ctime(filepath):
+            try:
+                return float(os.getxattr(filepath, b"user.loguru_crtime"))
+            except OSError:
+                return os.stat(filepath).st_mtime
 
-    def get_ctime(filepath):
-        return os.stat(filepath).st_birthtime
+        def set_ctime(filepath, timestamp):
+            try:
+                os.setxattr(filepath, b"user.loguru_crtime", str(timestamp).encode("ascii"))
+            except OSError:
+                pass
 
-    def set_ctime(filepath, timestamp):
-        pass
+    else:
 
-
-elif hasattr(os, "getxattr") and hasattr(os, "setxattr"):
-
-    def get_ctime(filepath):
-        try:
-            return float(os.getxattr(filepath, b"user.loguru_crtime"))
-        except OSError:
+        def get_ctime(filepath):
             return os.stat(filepath).st_mtime
 
-    def set_ctime(filepath, timestamp):
-        try:
-            os.setxattr(filepath, b"user.loguru_crtime", str(timestamp).encode("ascii"))
-        except OSError:
+        def set_ctime(filepath, timestamp):
             pass
 
+    return get_ctime, set_ctime
 
-else:
 
-    def get_ctime(filepath):
-        return os.stat(filepath).st_mtime
-
-    def set_ctime(filepath, timestamp):
-        pass
+get_ctime, set_ctime = load_ctime_functions()
