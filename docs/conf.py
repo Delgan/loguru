6d11ffd9175ced39c2050d85b68d0d699036fe36
Delgan <delgan.py@gmail.com>
2022-06-18 16:54:03 +0200

    Fix warning while generating HTML docs

--- docs/conf.py
+++ docs/conf.py
@@ -65,7 +65,7 @@ master_doc = "index"
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "English"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -107,8 +107,6 @@ html_static_path = ["_static"]
 #
 # html_sidebars = {}
 
-# Try to fix formatting with Sphinx v2
-html4_writer = True
 
 # -- Options for HTMLHelp output ---------------------------------------------
 
