c7777fb82be261c3ab2864d2962f5c428e606b7b
Pim Hoeven <pim.hoeven@heineken.com>
2022-09-14 15:20:48 +0000

    CICD fixes

--- tests/test_clrf_save.py
+++ tests/test_clrf_save.py
@@ -3,13 +3,16 @@ import pytest
 from loguru import logger
 
 
-@pytest.mark.parametrize("raw,original,replacement", (
-    (False, "\n", "\\n"),
-    (False, "\r", "\\r"),
-    (True, "\n", "\n"),
-    (True, "\r", "\r"),
-))
+@pytest.mark.parametrize(
+    "raw,original,replacement",
+    (
+        (False, "\n", "\\n"),
+        (False, "\r", "\\r"),
+        (True, "\n", "\n"),
+        (True, "\r", "\r"),
+    ),
+)
 def test_clrf(writer, raw, original, replacement):
     logger.add(writer, format="{message}")
-    logger.opt(raw=raw).debug(f"Line{original}Next line")
-    assert writer.read() == f"Line{replacement}Next line" + ("\n" if not raw else "")
+    logger.opt(raw=raw).debug("Line" + original + "Next line")
+    assert writer.read() == "Line" + replacement + "Next line" + ("\n" if not raw else "")
