3f6add235cbfeeeb6b9a3024c8ed8dae3a9b5114
Delgan <delgan.py@gmail.com>
2022-11-11 12:44:23 +0100

    Add more unit tests for deadlock prevention

--- tests/test_locks.py
+++ tests/test_locks.py
@@ -1,4 +1,8 @@
 import gc
+import pickle
+import sys
+import threading
+import time
 
 import pytest
 
@@ -62,3 +66,68 @@ def test_no_deadlock_on_generational_garbage_collection(_remove_cyclic_reference
 
     # THEN deadlock should not be reached
     assert True
+
+
+def test_no_deadlock_if_logger_used_inside_sink_with_catch(capsys):
+    def sink(message):
+        logger.info(message)
+
+    logger.add(sink, colorize=False, catch=True)
+
+    logger.info("Test")
+
+    out, err = capsys.readouterr()
+    assert out == ""
+    assert "deadlock avoided" in err
+
+
+def test_no_deadlock_if_logger_used_inside_sink_without_catch():
+    def sink(message):
+        logger.info(message)
+
+    logger.add(sink, colorize=False, catch=False)
+
+    with pytest.raises(RuntimeError, match=r".*deadlock avoided.*"):
+        logger.info("Test")
+
+
+def test_no_error_if_multithreading(capsys):
+    barrier = threading.Barrier(2)
+
+    def sink(message):
+        barrier.wait()
+        sys.stderr.write(message)
+        time.sleep(0.5)  # Give time to the other thread to try to acquire the lock.
+
+    def worker():
+        logger.info("Thread message")
+        barrier.wait()  # Release main thread.
+
+    logger.add(sink, colorize=False, catch=False, format="{message}")
+    thread = threading.Thread(target=worker)
+    thread.start()
+
+    barrier.wait()
+    logger.info("Main message")
+
+    out, err = capsys.readouterr()
+    assert out == ""
+    assert err == "Thread message\nMain message\n"
+
+
+def _pickle_sink(message):
+    sys.stderr.write(message)
+
+    if message.record["extra"].get("clone", False):
+        new_logger = pickle.loads(pickle.dumps(logger))
+        new_logger.bind(clone=False).info("From clone")
+
+
+def test_pickled_logger_does_not_inherit_acquired_local(capsys):
+    logger.add(_pickle_sink, colorize=False, catch=False, format="{message}")
+
+    logger.bind(clone=True).info("From main")
+
+    out, err = capsys.readouterr()
+    assert out == ""
+    assert err == "From main\nFrom clone\n"
