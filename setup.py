f615ace52f0e9ed30b0381d762ff5a5f407d4ccd
Delgan <delgan.py@gmail.com>
2023-01-06 19:50:01 +0100

    Add support for Python 3.11

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: Implementation :: PyPy",
         "Programming Language :: Python :: Implementation :: CPython",
