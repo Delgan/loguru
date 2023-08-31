6ce0fa571afaf3d5a68b354fc16c2a32e59004d2
Delgan <delgan.py@gmail.com>
2023-08-31 18:09:12 +0200

    Fix missing seconds when formatting timezone offset

--- loguru/_datetime.py
+++ loguru/_datetime.py
@@ -36,7 +36,7 @@ class datetime(datetime_):  # noqa: N801
         tzinfo = dt.tzinfo or timezone(timedelta(seconds=0))
         offset = tzinfo.utcoffset(dt).total_seconds()
         sign = ("-", "+")[offset >= 0]
-        h, m = divmod(abs(offset // 60), 60)
+        (h, m), s = divmod(abs(offset // 60), 60), abs(offset) % 60
 
         rep = {
             "YYYY": "%04d" % year,
@@ -69,8 +69,8 @@ class datetime(datetime_):  # noqa: N801
             "SSSSS": "%05d" % (microsecond // 10),
             "SSSSSS": "%06d" % microsecond,
             "A": ("AM", "PM")[hour // 12],
-            "Z": "%s%02d:%02d" % (sign, h, m),
-            "ZZ": "%s%02d%02d" % (sign, h, m),
+            "Z": "%s%02d:%02d%s" % (sign, h, m, (":%09.06f" % s)[: 11 if s % 1 else 3] * (s > 0)),
+            "ZZ": "%s%02d%02d%s" % (sign, h, m, ("%09.06f" % s)[: 10 if s % 1 else 2] * (s > 0)),
             "zz": tzinfo.tzname(dt) or "",
             "X": "%d" % timestamp,
             "x": "%d" % (int(timestamp) * 1000000 + microsecond),
