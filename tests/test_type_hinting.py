cdb340e98258709e3f0d0a6d4a09d2ad33627a5f
Delgan <delgan.py@gmail.com>
2023-09-22 18:51:38 +0200

    Add pyproject.toml to handle most tool configs
    I considered moving Tox config to pyproject.toml as well, but it's not
    fully supported and requires passing the INI config as a string in the
    "legacy_tox_ini" entry. That doesn't look good, therefore we'll stay
    with that additional file unfortunately.

--- tests/test_type_hinting.py
+++ tests/test_type_hinting.py
@@ -4,5 +4,5 @@ import mypy.api
 def test_mypy_import():
     # Check stub file is valid and can be imported by Mypy.
     # There exist others tests in "typesafety" subfolder but they aren't compatible with Python 3.5.
-    _, _, result = mypy.api.run(["-c", "from loguru import logger"])
+    _, _, result = mypy.api.run(["--strict", "-c", "from loguru import logger"])
     assert result == 0
