808a07ae0473fd222eb0ce7c559c42ace9409826
Delgan <delgan.py@gmail.com>
2024-11-24 14:50:28 +0100

    Add stderr of failed Mypy command to tests output

--- tests/test_type_hinting.py
+++ tests/test_type_hinting.py
@@ -1,8 +1,11 @@
+import sys
+
 import mypy.api
 
 
 def test_mypy_import():
     # Check stub file is valid and can be imported by Mypy.
     # There exist others tests in "typesafety" subfolder but they aren't compatible with Python 3.5.
-    _, _, result = mypy.api.run(["--strict", "-c", "from loguru import logger"])
+    out, _, result = mypy.api.run(["--strict", "-c", "from loguru import logger"])
+    print("".join(out), file=sys.stderr)
     assert result == 0
