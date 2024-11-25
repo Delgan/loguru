74853f6887640031b6db3bdd01ee58245dbaee66
Trim21 <trim21.me@gmail.com>
2024-11-25 18:05:16 +0800

    lazy

--- tests/test_lazy.py
+++ tests/test_lazy.py
@@ -1,9 +1,9 @@
-from loguru import LazyValue, logger
+from loguru import logger
 
 
 def test_lazy_value(writer):
     logger_bound = logger.bind(a=0)
     logger_bound.add(writer, format="{message}")
-    logger_bound.info("hello {}", LazyValue(str, 1))
+    logger_bound.info("hello {}", logger.lazy(str, 1))
 
     assert writer.read() == "hello 1\n"
