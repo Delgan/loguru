24c551305fbe7bc240460c5eb08bc2bdacf79f62
Delgan <delgan.py@gmail.com>
2024-12-01 19:58:54 +0100

    Disable redundant version selector in RTD theme

--- docs/conf.py
+++ docs/conf.py
@@ -86,7 +86,9 @@ html_theme = "sphinx_rtd_theme"
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {}
+html_theme_options = {
+    "version_selector": False,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
