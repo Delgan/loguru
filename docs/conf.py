57dbda17cf7d382319020341f0bcafe6587919f0
Delgan <delgan.py@gmail.com>
2024-12-01 20:02:28 +0100

    Re-enable version selector of RDT theme
    I thought it would just remove the selector, but it also hide the
    version subtitle, which I think is useful.

--- docs/conf.py
+++ docs/conf.py
@@ -86,9 +86,7 @@ html_theme = "sphinx_rtd_theme"
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {
-    "version_selector": False,
-}
+html_theme_options = {}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
