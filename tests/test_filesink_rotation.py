b64e1d537ffb367e419eaab784920f7d1a809409
Collin Heist <collinheist@gmail.com>
2025-01-17 10:06:04 -0700

    Allow list of rotation conditions in file rotation logic (#1175)

--- tests/test_filesink_rotation.py
+++ tests/test_filesink_rotation.py
@@ -1058,6 +1058,36 @@ def test_recipe_rotation_both_size_and_time(freeze_time, tmp_path):
     )
 
 
+def test_multiple_rotation_conditions(freeze_time, tmp_path):
+    with freeze_time("2020-01-01 20:00:00") as frozen:
+        logger.add(tmp_path / "file.log", rotation=["8 B", "1 min"], format="{message}")
+        logger.info("abcde")
+        frozen.tick()
+
+        logger.info("fghij")
+        frozen.tick()
+
+        logger.info("klm")
+        frozen.move_to("2020-01-01 20:01:01")
+
+        logger.info("no")
+
+    check_dir(
+        tmp_path,
+        files=[
+            ("file.2020-01-01_20-00-00_000000.log", "abcde\n"),
+            ("file.2020-01-01_20-00-01_000000.log", "fghij\n"),
+            ("file.2020-01-01_20-00-02_000000.log", "klm\n"),
+            ("file.log", "no\n"),
+        ],
+    )
+
+
+def test_empty_rotation_condition_list():
+    with pytest.raises(ValueError, match="^Must provide at least one rotation condition$"):
+        logger.add("test.log", rotation=[])
+
+
 @pytest.mark.parametrize(
     "rotation", [object(), os, datetime.date(2017, 11, 11), datetime.datetime.now(), 1j]
 )
