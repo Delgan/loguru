c401b0c83757a34c275fbb45090abfe0774f687f
Delgan <delgan.py@gmail.com>
2026-04-06 12:41:42 +0200

    Drop unit testing of older Mypy versions
    We rely on `pytest-mypy-plugins` to test the type hints stub file
    against Mypy analyzer. This plugin compares raw output of Mypy from
    expected to actual results.
    
    Until then, we were running these unit tests down to Python 3.6. The
    plugin and Mypy stopped supporting these older versions a long time ago.
    Therefore, we needed to use different versions of them according to the
    Python version tested. Obviously, this causes different Mypy output,
    and since the plugin rely in this output, we needed to tweak the output
    so that it matches for every cases.
    
    First, this was done we appropriate "skip" conditions and duplicated
    tests (with output slightly changed). Then, the tests were unified and
    the output programmatically adapted in `conftest.py`. However, we're
    reaching a point where it's becoming very difficult to do so.
    
    With Mypy v1.20.0, the tests definition can no longer be adapted easily
    to fit all previous versions. In particular, the `force_union_syntax`
    will disappear. And I don't want to go back to duplicating
    everything.
    
    Actually, it's not technically sound to try to tests Mypy output this
    way. The `pytest-mypy-plugins` isn't meant to be used this way.
    Obviously, it's tied to a specific Mypy version and can't cover all
    cases.
    
    Additionally, it makes not much sense to test for older Mypy versions.
    Users are generally expected to use the last one, and they can even
    use it to verify type safety of older code bases.
    
    Therefore, rather than trying to run Mypy unit tests for all Python
    versions, we restrict this to the ones supported by Mypy. That seems
    more pragmatic. We're are still ensuring type hints are usable with
    older versions (see `test_type_hinting.py`), but from now on, we'll use
    a single version of Mypy for exhaustive unit tests (`test_logger.yml`).
    
    This also cleanup and simplify `conftests.py`.

--- tests/test_type_hinting.py
+++ tests/test_type_hinting.py
@@ -5,7 +5,7 @@ import mypy.api
 
 def test_mypy_import():
     # Check stub file is valid and can be imported by Mypy.
-    # There exist others tests in "typesafety" subfolder but they aren't compatible with Python 3.5.
+    # There exist others tests in "typesafety" subfolder but they require a recent Python version.
     out, _, result = mypy.api.run(["--strict", "-c", "from loguru import logger"])
     print("".join(out), file=sys.stderr)
     assert result == 0
