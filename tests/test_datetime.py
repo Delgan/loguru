f4b25c3cbff623a6bab09d2b9728cbf2b762ee9c
Delgan <delgan.py@gmail.com>
2026-08-29 18:20:34 +0200

    Fix incorrect formatting of negative timezone offsets

--- tests/test_datetime.py
+++ tests/test_datetime.py
@@ -140,7 +140,8 @@ def test_formatting(writer, freeze_time, time_format, date, timezone, expected):
     [
         ("%Y-%m-%d %H-%M-%S %f %Z %z", 7230.099, "2018-06-09 01-02-03 000000 ABC +020030.099000"),
         ("YYYY-MM-DD HH-mm-ss zz Z ZZ", 6543, "2018-06-09 01-02-03 ABC +01:49:03 +014903"),
-        ("HH-mm-ss zz Z ZZ", -12345.06702, "01-02-03 ABC -03:26:45.067020 -032645.067020"),
+        ("HH-mm-ss zz Z ZZ", -12345.06702, "01-02-03 ABC -03:25:45.067020 -032545.067020"),
+        ("YYYY-MM-DD HH-mm-ss zz Z ZZ", -3661, "2018-06-09 01-02-03 ABC -01:01:01 -010101"),
     ],
 )
 @pytest.mark.skipif(sys.version_info < (3, 7), reason="Offset must be a whole number of minutes")
